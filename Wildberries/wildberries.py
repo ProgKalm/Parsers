40fbf46563af056fa05b74a32c42a24a6e227846
Ilya Suponev <suponeff.i@gmail.com>
2022-11-05 14:34:52 +0300

    update_wildberries_parser_dir

--- Wildberries/wildberries.py
+++ Wildberries/wildberries.py
@@ -1,3 +1,5 @@
+import json
+import os.path
 import time, fake_useragent
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -28,12 +30,12 @@ class SortMode:
 class Wildberries:
     __host = "https://www.wildberries.ru/"
     __options_params = {
-        "--user-agent": f"{fake_useragent.UserAgent().random}"
+        "--user-agent": f"{fake_useragent.UserAgent().random}",
+        "--disable-blink-features": "AutomationControlled"
     }
-    __ALL_ITEM_DETAILS_BUTTON = '/html/body/div[1]/main/div[2]/div/div[3]/div/div[3]' \
-                                '/section/div[3]/div[1]/div/div[2]/div[2]/button'
+    __ALL_ITEM_DETAILS_BUTTON = '/html/body/div[1]/main/div[2]/div/div[3]/div/div[3]/section/div[2]/section[1]/div[2]/div[2]/div'
 
-    def __init__(self):
+    def __init__(self, user: str = __name__):
         options = webdriver.ChromeOptions()
         for option in self.__options_params:
             options.add_argument(f"{option}={self.__options_params[option]}")
@@ -43,6 +45,7 @@ class Wildberries:
         )
         self.__sort_mode = SortMode.POPULAR
         self.__max_page_count = 3
+        self.__user = user
 
     def set_max_page_cout(self, count: int = 1):
         if count <= 1:
@@ -57,12 +60,15 @@ class Wildberries:
         try:
             data = []
             items_urls = self.__pages_parse(search_request)
-            print(len(items_urls))
+
             for item_url in items_urls:
+                print(f"Url: {item_url}")
                 item_data = self.__go_to_item(item_url)
                 if item_data is not None:
                     data.append(item_data)
-                    print(item_data)
+
+            if len(data) > 0:
+                self.__save_result(data)
 
         except Exception as ex:
             print(ex)
@@ -73,8 +79,9 @@ class Wildberries:
         page = 1
         items_urls = []
         while page <= self.__max_page_count:
+            print(f"Page: {page}")
             self.__go_to_page(page, search_request)
-            time.sleep(2)
+            time.sleep(1)
             items_list = self.__driver.find_element(By.CLASS_NAME, "product-card-overflow")
             items_list = items_list.find_elements(By.CLASS_NAME, "product-card__wrapper")
             for item in items_list:
@@ -98,7 +105,7 @@ class Wildberries:
             # find name
             item_name = item.find_element(By.CLASS_NAME, "product-page__header-wrap").find_element(By.TAG_NAME, "h1")
             item_data.setdefault("name", item_name.text)
-
+            print(f"Name: {item_name}")
             # find price
             item_price = item.find_element(By.CLASS_NAME, "product-page__price-block")
             item_price = item_price.find_element(By.CLASS_NAME, "price-block")
@@ -106,10 +113,10 @@ class Wildberries:
             item_price = item_price.find_element(By.TAG_NAME, "p").find_element(By.TAG_NAME, "ins")
             item_price = to_int(item_price.text)
             item_data.setdefault("price", item_price)
-
+            print(f"Price: {item_price}")
             # find details
             item_details = item.find_element(By.CLASS_NAME, "product-page__details-section")
-            item_details.find_element(By.XPATH, self.__ALL_ITEM_DETAILS_BUTTON).click()
+            self.__driver.find_element(By.XPATH, self.__ALL_ITEM_DETAILS_BUTTON).click()
             item_details = item.find_element(By.CLASS_NAME, "product-page__details-section")
             item_details = item_details.find_element(By.CLASS_NAME, "product-params")
             item_details = item_details.find_elements(By.TAG_NAME, "table")
@@ -125,6 +132,7 @@ class Wildberries:
                     detail_data.setdefault(key, value)
                 details_data.setdefault(detail_name, detail_data)
             item_data.setdefault("details", details_data)
+            print(f"Details:  {details_data}")
             time.sleep(3)
             return item_data
         except Exception as ex:
@@ -135,10 +143,22 @@ class Wildberries:
         req = f"{self.__host}catalog/0/search.aspx?page={page}&sort={self.__sort_mode}&search={search_request}"
         self.__driver.get(req)
 
+    def __save_result(self, results):
+        self.see_dir("./results/")
+        file_name = f"{self.__user.strip('_')}.json"
+        with open(f"./results/{file_name}", "w", encoding="utf-8") as file:
+            file.write(json.dumps(results))
+
     @staticmethod
     def __is_valid_request(search_request: str):
         return isinstance(search_request, str) and search_request is not None and search_request.strip() != ""
 
+    @staticmethod
+    def see_dir(path):
+        if not os.path.exists(path):
+            os.mkdir(path)
+
 
 w = Wildberries()
+w.set_max_page_cout(1)
 w.find("смартфоны")
